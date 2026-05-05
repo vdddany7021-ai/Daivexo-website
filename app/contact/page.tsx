095d5b04c0082171e27a181ffc3bd51f559db11e
vdddany7021-ai <vdd.dany7021@gmail.com>
2026-05-05 20:46:32 +0200

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -39,7 +39,7 @@ export default function ContactPage() {
             <div>
               <p className="text-sm text-gray-400">Address</p>
               <p>
-                Leernsesteenweg 124 A<br />
+                Leernsesteenweg 124A<br />
                 9800 Deinze<br />
                 België
               </p>
